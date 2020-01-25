c2b6ed3ed27d4fe8aa9cb8486116e5f014074749
mintzlee <mintz022@umn.edu>
2020-01-25 17:36:07 -0600

    Test passes. Fixed expected hello output

--- src/hello_test.go
+++ src/hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 	var returnedString = hello()
 
-	if returnedString != "hello, world\n" {
+	if returnedString != "Hello, world\n" {
 		t.Errorf("Expected \"Hello, world\\n\", but received: %s", returnedString)
 	}
 }
